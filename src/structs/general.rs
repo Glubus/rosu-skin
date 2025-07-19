f8b811ac25e9c69f3f7eee018b1783b5873b4c56
Glubus <50545507+Glubus@users.noreply.github.com>
2025-07-19 18:48:32 +0200

    feat: added default for general

--- src/structs/general.rs
+++ src/structs/general.rs
@@ -2,26 +2,64 @@
 
 struct General {
     name: String,
-    author: String,
+    author: Option<String>,
     version: String,
     cursor: Cursor,
     slider: Slider,
-    animation_framerate: u8,
+    animation_framerate: i8,
     hit_circle_overlay_above_number: bool, 
     layered_hit_sounds: bool,
+    combo_burst: ComboBurst,
+    spinner: Spinner,
+}
+
+impl Default for General {
+
+    fn default() -> Self {
+        Self {
+            name: String::from("Unknown"),
+            author: None,
+            version: String::from("latest"),
+            cursor: Cursor::default(),
+            slider: Slider::default(),
+            animation_framerate: -1,
+            hit_circle_overlay_above_number: true,
+            layered_hit_sounds: true,
+            combo_burst: ComboBurst::default(),
+            spinner: Spinner::default(),
+        }
+    }
 }
 
 struct ComboBurst
 {
     burst_random: bool, // 0 = no, 1 = yes
-    custom_burst_sounds: u16,
+    custom_burst_sounds: Option<u16>,
 }
-
+impl Default for ComboBurst {
+    fn default() -> Self {
+        Self {
+            burst_random: false,
+            custom_burst_sounds: None,
+        }
+    }
+}
 struct Cursor {
     centre: bool,
     expand: bool,
     rotate: bool,
     trail_rotate: bool,
+}
+
+impl Default for Cursor {
+    fn default() -> Self {
+        Self {
+            centre: true,
+            expand: true,
+            rotate: true,
+            trail_rotate: true,
+        }
+    }
 }
 
 struct Slider
@@ -30,9 +68,28 @@ struct Slider
     ball_tint: bool,
 }
 
+impl Default for Slider {
+    fn default() -> Self {
+        Self {
+            ball_flip: true,
+            ball_tint: false,
+        }
+    }
+}
+
 struct Spinner
 {
     fade_playfield: bool,
     frequency_modulate: bool,
     no_blink: bool,
+}
+
+impl Default for Spinner {
+    fn default() -> Self {
+        Self {
+            fade_playfield: false,
+            frequency_modulate: true,
+            no_blink: false,
+        }
+    }
 }
